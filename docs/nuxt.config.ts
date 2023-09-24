f6427eed705c5af94c7004367727e7117864b121
Blaquewithaq <blaquewithaq@gmail.com>
2023-09-24 11:26:57 -0500

    updated nuxt.config.ts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,8 +1,42 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // General
   app: {
-    baseURL: process.env.NODE_ENV === "development" ? '/' : '/projectm/',
-    buildAssetsDir: 'assets'
+    baseURL: process.env.NODE_ENV === "development" ? "/" : "/projectm/",
+    buildAssetsDir: "assets",
   },
-  modules: ['@nuxt/content']
-})
+  experimental: {
+    payloadExtraction: false,
+  },
+  modules: ["@nuxt/content"],
+  router: {
+    options: {
+      strict: false,
+    },
+  },
+  sourcemap: false,
+  ssr: true,
+
+  // Modules
+  content: {
+    // Configuring code highlighting
+    // https://content.nuxtjs.org/api/configuration
+    highlight: {
+      theme: "github-dark",
+      preload: ["c", "cpp"],
+    },
+    markdown: {
+      // Configuring external link processing
+      // https://github.com/rehypejs/rehype-external-links
+      rehypePlugins: [
+        [
+          "rehype-external-links",
+          {
+            target: "_blank",
+            rel: "noopener noreferer",
+          },
+        ],
+      ],
+    },
+  },
+});
